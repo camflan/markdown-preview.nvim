625ee893d35b57d5dc1425945b65eecdba73d6e6
Camron Flanders <me@camronflanders.com>
2024-11-05 11:33:17 -0600

    update mermaid

--- app/pages/mermaid.js
+++ app/pages/mermaid.js
@@ -1,35 +1,40 @@
-import {escape} from './utils';
+import { escape } from "./utils";
 
 /*
  * global mermaid
-*/
+ */
 const mermaidChart = (code) => {
   try {
     // eslint-disable-next-line
-    mermaid.parse(code)
-    return `<div class="mermaid">${escape(code)}</div>`
+    mermaid.parse(code);
+    return `<div class="mermaid">${escape(code)}</div>`;
   } catch ({ str, hash }) {
-    return `<pre>${str}</pre>`
+    return `<pre>${str}</pre>`;
   }
-}
+};
+
+const VALID_MERMAID_CHART_TYPES = ["gantt", "sequenceDiagram", "erDiagram"];
 
 const MermaidPlugin = (md) => {
-  const origin = md.renderer.rules.fence.bind(md.renderer.rules)
+  const origin = md.renderer.rules.fence.bind(md.renderer.rules);
   md.renderer.rules.fence = (tokens, idx, options, env, slf) => {
-    const token = tokens[idx]
-    const code = token.content.trim()
-    if (typeof token.info === 'string' && token.info.trim() === 'mermaid') {
-      return mermaidChart(code)
-    }
-    const firstLine = code.split(/\n/)[0].trim()
-    if (firstLine === 'gantt' ||
-      firstLine === 'sequenceDiagram' ||
-      firstLine === 'erDiagram' ||
-      firstLine.match(/^graph (?:TB|BT|RL|LR|TD);?$/)) {
-      return mermaidChart(code)
-    }
-    return origin(tokens, idx, options, env, slf)
-  }
-}
+    const token = tokens[idx];
+    const code = token.content.trim();
 
-export default MermaidPlugin
+    if (typeof token.info === "string" && token.info.trim() === "mermaid") {
+      return mermaidChart(code);
+    }
+
+    const firstLine = code.split(/\n/)[0].trim();
+    if (
+      VALID_MERMAID_CHART_TYPES.includes(firstLine) ||
+      firstLine.match(/^graph (?:TB|BT|RL|LR|TD);?$/)
+    ) {
+      return mermaidChart(code);
+    }
+
+    return origin(tokens, idx, options, env, slf);
+  };
+};
+
+export default MermaidPlugin;
